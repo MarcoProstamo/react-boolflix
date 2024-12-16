8b88500ddcb346a1162c372cc77b6dc0f63ea0e4
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-16 11:44:12 +0100

    Add → Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./assets/layout/Header";
 import Main from "./assets/layout/Main";
 export default function App() {
   return (
-    <div className="wrapper">
+    <div className="wrapper" data-bs-theme="dark">
       <Header />
       <Main />
     </div>
