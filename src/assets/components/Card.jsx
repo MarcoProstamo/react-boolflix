e5b4655b66877fbab8ba06c3a561d3c0596243e4
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-16 16:32:13 +0100

    Feat → Star Raiting + Image Render

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,3 +1,40 @@
-export default function Card() {
-  return <h1>Card</h1>;
+export default function Card({ item }) {
+  const posterOrigin = import.meta.env.VITE_POSTER_URL;
+  const posterSize = import.meta.env.VITE_POSTER_SIZE;
+  const posterPath = item.poster_path;
+  let posterUrl;
+  if (posterPath) posterUrl = posterOrigin + posterSize + posterPath;
+
+  function raiting(vote) {
+    if (!vote || vote < 0) vote = 0;
+    const stars = Math.round(vote / 2);
+    const totalStars = 5;
+    const starsArr = [];
+    for (let i = 0; i < totalStars; i++) {
+      i < stars
+        ? starsArr.push(<i key={i} className="fa-solid fa-star"></i>)
+        : starsArr.push(<i key={i} className="fa-regular fa-star"></i>);
+    }
+    return starsArr;
+  }
+
+  return (
+    <div className="card p-0 cursor-pointer rounded">
+      <img src={posterUrl} className="card-img img-fluid" alt="No Image 😢" />
+      <div className="overlay">
+        <p>
+          <strong>Title: </strong> {item.title || item.name}
+        </p>
+        <p>
+          <strong>Original Title: </strong>
+          {item.original_title || item.original_name}
+        </p>
+        <p>{raiting(item.vote_average)}</p>
+        <p>
+          <strong>Language: </strong>
+          {item.original_language}
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/assets/layout/Main.jsx
+++ src/assets/layout/Main.jsx
@@ -1,55 +1,46 @@
 import { UseMovieTvContext } from "../contexts/MovieTvContext";
-
+import Card from "../components/Card";
 export default function Main() {
   const { movies, series, getMovieOriginCountry, movieOriginCountry } =
     UseMovieTvContext();
 
   return (
     <main>
-      {/* Movie or TV Series Exists? */}
-      {(!series.results || series.results.length === 0) && (
-        <h2>No TV Series Found</h2>
-      )}
-      {(!movies.results || movies.results.length === 0) && (
-        <h2>No Movies Found</h2>
-      )}
+      <div className="container">
+        {/* Movie or TV Series Exists? */}
+        <div className="mt-5 d-flex justify-content-center align-items-center flex-column">
+          {(!series.results || series.results.length === 0) && (
+            <h2 className="fw-semibold text-terziary">No TV Series Found</h2>
+          )}
+          {(!movies.results || movies.results.length === 0) && (
+            <h2 className="fw-semibold text-terziary">No Movies Found</h2>
+          )}
+        </div>
 
-      {/* Movie Output */}
-      {movies.results && (
-        <>
-          <h1>Movies</h1>
-          <ul>
-            {movies.results.map((movie) => (
-              <li key={movie.id}>
-                <p>{movie.title}</p>
-                <p>{movie.original_title}</p>
-                <img src={`https://flagsapi.com/${"BR"}/shiny/32.png`} />
-                <p>{movie.vote_average}</p>
-                {console.log()}
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
+        {/* Movie Output */}
+        {movies.results && series.results.length > 0 && (
+          <section className="my-5">
+            <h2 className="fs-1 text-center fw-semibold">Movies</h2>
+            <div className="row row-cols-3 gap-1">
+              {movies.results.map((movie) => (
+                <Card key={movie.id} item={movie} />
+              ))}
+            </div>
+          </section>
+        )}
 
-      {/* TV Series Output */}
-      {series.results && (
-        <>
-          <h1>TV Series</h1>
-          <ul>
-            {series.results.map((serie) => (
-              <li key={serie.id}>
-                <p>{serie.name}</p>
-                <p>{serie.original_name}</p>
-                <img
-                  src={`https://flagsapi.com/${serie.origin_country}/shiny/32.png`}
-                />
-                <p>{serie.vote_average}</p>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
+        {/* TV Series Output */}
+        {series.results && series.results.length > 0 && (
+          <section className="my-5">
+            <h2 className="fs-1 text-center fw-semibold">TV Series</h2>
+            <div className="row row-cols-3 gap-1">
+              {series.results.map((serie) => (
+                <Card key={serie.id} item={serie} />
+              ))}
+            </div>
+          </section>
+        )}
+      </div>
     </main>
   );
 }
