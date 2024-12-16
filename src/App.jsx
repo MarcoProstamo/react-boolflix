ce8dd7988263729c8f1e23f0eb8fb8d0dda168ee
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-16 11:38:21 +0100

    Add → Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Header from "./assets/layout/Header";
+import Main from "./assets/layout/Main";
 export default function App() {
-  return;
+  return (
+    <div className="wrapper">
+      <Header />
+      <Main />
+    </div>
+  );
 }
