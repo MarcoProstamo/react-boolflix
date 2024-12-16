4208f32da10dad516024547006fb13dcca253b1a
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-16 14:04:28 +0100

    Feat → TV Series Dynamic Flag

--- src/assets/contexts/MovieTvContext.jsx
+++ src/assets/contexts/MovieTvContext.jsx
@@ -7,6 +7,7 @@ export const UseMovieTvContext = () => useContext(MovieTvContext);
 // # Context Provider
 export function MovieTvContextProvider({ children }) {
   const movieUrl = import.meta.env.VITE_API_URL_MOVIE;
+  const movieDetailUrl = import.meta.env.VITE_API_URL_MOVIE_DETAILS;
   const serieUrl = import.meta.env.VITE_API_URL_SERIE;
   const options = {
     method: "GET",
@@ -21,11 +22,28 @@ export function MovieTvContextProvider({ children }) {
     getMovies: function (term) {
       fetch(`${movieUrl}?query=${term}`, options)
         .then((res) => res.json())
-        .then((data) =>
-          setMovieTvData((movieTvData) => ({ ...movieTvData, movies: data }))
-        )
+        .then((data) => {
+          //   movieTvData.getMovieOriginCountry(data.results.id);
+          setMovieTvData((movieTvData) => ({
+            ...movieTvData,
+            movies: data,
+            // origin_country: movieTvData.movieOriginCountry,
+          }));
+        })
         .catch((err) => console.error(err));
     },
+    // movieOriginCountry: "",
+    // getMovieOriginCountry: function (movieId) {
+    //   fetch(`${movieDetailUrl}/${movieId}`, options)
+    //     .then((res) => res.json())
+    //     .then((data) =>
+    //       setMovieTvData((movieTvData) => ({
+    //         ...movieTvData,
+    //         movieOriginCountry: data.origin_country,
+    //       }))
+    //     )
+    //     .catch((err) => console.error(err));
+    // },
     series: [],
     getSeries: function (term) {
       fetch(`${serieUrl}?query=${term}`, options)
--- src/assets/layout/Main.jsx
+++ src/assets/layout/Main.jsx
@@ -1,7 +1,8 @@
 import { UseMovieTvContext } from "../contexts/MovieTvContext";
 
 export default function Main() {
-  const { movies, series } = UseMovieTvContext();
+  const { movies, series, getMovieOriginCountry, movieOriginCountry } =
+    UseMovieTvContext();
 
   return (
     <main>
@@ -22,8 +23,9 @@ export default function Main() {
               <li key={movie.id}>
                 <p>{movie.title}</p>
                 <p>{movie.original_title}</p>
-                <p>{movie.original_language}</p>
+                <img src={`https://flagsapi.com/${"BR"}/shiny/32.png`} />
                 <p>{movie.vote_average}</p>
+                {console.log()}
               </li>
             ))}
           </ul>
@@ -39,7 +41,9 @@ export default function Main() {
               <li key={serie.id}>
                 <p>{serie.name}</p>
                 <p>{serie.original_name}</p>
-                <p>{serie.original_language}</p>
+                <img
+                  src={`https://flagsapi.com/${serie.origin_country}/shiny/32.png`}
+                />
                 <p>{serie.vote_average}</p>
               </li>
             ))}
